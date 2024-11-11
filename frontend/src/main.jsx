59a4a770143b8787d233ec32c6024805cd759c52
TROGER-VU <ayushgupta91331@gmail.com>
2024-11-11 10:44:58 +0530

    loginsignupdone

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -3,11 +3,11 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import '@fortawesome/fontawesome-free/css/all.min.css';
-
+import 'react-toastify/ReactToastify.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+      <App />
   </React.StrictMode>
 );
